8d465919915354b14385930dcee4b237c51f1e51
Joshua Zhao <joshua.zhao0315@gmail.com>
2021-10-11 15:42:06 -0700

    Fix some insects

---
+++ frontend/src/components/nav/navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  
+      <div>
+        <h1>Nav Bar</h1>
+      </div>
+    );
+  }
+}
+ 
+export default NavBar;
