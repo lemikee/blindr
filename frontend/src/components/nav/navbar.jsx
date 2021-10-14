2eb1aba7c53e750b2a6f1c9cc099c1ec248d886b
Mike Le <letumichael@gmail.com>
2021-10-14 13:47:10 -0700

    Refactored to functional component

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,30 +1,21 @@
-import React from 'react';
-import { FaUser, FaRegAddressCard } from 'react-icons/fa';
-import MatchChat from './match_chat';
+import React, { useState } from "react";
+import { FaUser, FaRegAddressCard } from "react-icons/fa";
+import MatchChat from "./match_chat";
 
-class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
+export default function NavBar(props) {
+  const [chats, setChats] = useState(["Facebook", "Yahoo", "Google", "Test"]);
 
-    }
-  }
-  render() { 
-    let chats = ['Facebook', 'Yahoo', 'Google', 'Facebook', 'Yahoo', 'Google', 'Facebook', 'Yahoo', 'Google', 'Facebook', 'Yahoo', 'Google', 'Facebook', 'Yahoo', 'Google'];
-    return (  
-      <div className='navbar'>
-        <div className='navbar-header'>
-          <FaUser className='profile-btn'/>
-          <FaRegAddressCard className='profile-btn matches'/>
-        </div>
-        <div className='match-chats'>
-          {chats.map(chat => {
-            return <MatchChat company={chat} />
-          })}
-        </div>
+  return (
+    <div className="navbar">
+      <div className="navbar-header">
+        <FaUser className="profile-btn" />
+        <FaRegAddressCard className="profile-btn matches" />
       </div>
-    );
-  }
+      <div className="match-chats">
+        {chats.map((chat) => {
+          return <MatchChat company={chat} />;
+        })}
+      </div>
+    </div>
+  );
 }
- 
-export default NavBar;
