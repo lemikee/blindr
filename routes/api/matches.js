3f0247d7a2b0cc1bc4a7934a5451a7dc0b19a500
Josh Kim-Biggs <joshgykim@gmail.com>
2021-10-14 15:58:23 -0700

    Create and Destroy Match routes

--- routes/api/matches.js
+++ routes/api/matches.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Match = require("../../models/Match");
+const Job = require("../../models/Job");
 const bcrypt = require("bcryptjs");
 const keys = require("../../config/keys");
 const jwt = require("jsonwebtoken");
@@ -8,19 +9,52 @@ const jwt = require("jsonwebtoken");
 router.post("/createMatch", (req, res) => {
 
   Match.findOne({ user: req.body.userId })
-  
+    .then( match => {
+      match.jobs.push(req.body.jobId)
+      
+      Job.findOne({ _id: req.body.jobId})
+        .then( job => {
+          const payload = {
+            [req.body.jobId]: job
+          };
+          
+          return res.json({ match: payload });
+        })
+    })
+
 });
+
+router.delete("/deleteMatch", (req, res) => {
+
+  Match.findOne({ user: req.body.userId })
+    .then( match => {
+      
+      const jobIdx = match.jobs.indexOf(req.body.jobId)
+      match.jobs.splice(jobIdx, 1)
+
+      return res.json({ match: "Deleted successfully"});
+    })
+})
 
 // Takes userId, returns all matches and their associated job descriptions.
 router.get("/getMatches/:userId", (req, res) => {
 
   Match.findOne({ userId: req.params.userId })
     .then( match => {
-      const jobArr = match.jobs;
+      Job.find({ _id: { $in: match.jobs } })
+        .then( jobs => {
+          const payload = {};
 
-      
+          jobs.forEach( job => {
+            payload[job.id] = job;
+          })
+
+          return res.json({ match: payload });
+
+        });
     })
 
 })
+
 
 module.exports = router;
