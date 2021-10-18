27307f8ec11e5b0bc175e4b73ac4ec56d5f9196e
josephafern <60451011+josephafern@users.noreply.github.com>
2021-10-17 20:17:43 -0400

    MatchChat and Rec Card styling updated.

--- frontend/src/components/nav/match_chat.jsx
+++ frontend/src/components/nav/match_chat.jsx
@@ -4,6 +4,7 @@ import Chat from "./chat";
 import ChatF from "./chats_rooms/chats/chat_facebook";
 import ChatG from "./chats_rooms/chats/chat_google";
 import ChatY from "./chats_rooms/chats/chat_yahoo";
+import {BsPersonCircle} from 'react-icons/bs';
 
 class MatchChat extends React.Component {
   constructor(props) {
@@ -20,6 +21,7 @@ class MatchChat extends React.Component {
   }
 
   render() {
+    console.log(this.props.match)
     return (
       <div
         className={
@@ -28,22 +30,29 @@ class MatchChat extends React.Component {
             : "match-chat-container"
         }
       >
-        <div className="match-chat">
-          <p>{this.props.company}</p>
+        <div className="match-chat" onClick={this.toggleDropdown}>
+          <div>
+          <div style={{display: 'flex', alignItems: 'center'}}>
+          <BsPersonCircle className='matchchat-photo' />
+          <span>{this.props.match.company}</span>
+          <br/>
+          </div>
+          <p>{this.props.match.title}</p>
+          </div>
           <FaChevronDown
             style={
-              !this.state.dropdown ? { display: "block" } : { display: "none" }
+              !this.state.dropdown ? { transform: "rotate(180deg)" } : { transform: "rotate(0deg)" }
             }
             className="chevron"
-            onClick={this.toggleDropdown}
+            
           />
-          <FaChevronUp
+          {/* <FaChevronUp
             style={
               this.state.dropdown ? { display: "block" } : { display: "none" }
             }
             className="chevron"
-            onClick={this.toggleDropdown}
-          />
+            
+          /> */}
         </div>
         <Chat dropdown={this.state} />
       </div>
