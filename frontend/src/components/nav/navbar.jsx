d25c5c4e2c62ec463b5586cda8f75ac917d144e0
Joshua Zhao <joshua.zhao0315@gmail.com>
2021-10-28 16:46:44 -0700

    Logout button on nav bar

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { FaUser, FaRegAddressCard } from "react-icons/fa";
+import {BiLogOut} from 'react-icons/bi';
 import MatchChat from "./match_chat";
 import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
 import MatchChatFacebook from "./chats_rooms/matches/match_chat_facebook";
 import MatchChatYahoo from "./chats_rooms/matches/match_chat_yahoo";
 import MatchChatGoogle from "./chats_rooms/matches/match_chat_google";
@@ -11,46 +13,33 @@ import MatchChatGitlab from "./chats_rooms//matches/match_chat_gitlab";
 import MatchChatInsta from "./chats_rooms/matches/match_chat_instacart";
 import MatchChatRemix from "./chats_rooms/matches/match_chat_remix";
 
-// export default function NavBar(props) {
-//   // const [chats, setChats] = useState(["Facebook", "Yahoo", "Google"]);
-
-  
-//   return (
-//     <div className="navbar">
-//       <div className="navbar-header">
-//         <Link to="/profile"><FaUser className="profile-btn" /></Link>
-//         <Link to="/recs"><FaRegAddressCard className="profile-btn matches" /></Link>  
-//       </div>
-
-//       <div className="match-chats">
-//         <MatchChatFacebook company={"Facebook"} />
-//         <MatchChatYahoo company={"Yahoo"} />
-//         <MatchChatGoogle company={"Google"} />
-//         <MatchChatApple company={"Apple"} />
-//         <MatchChatCoin company={"Coinbase"} />
-//         <MatchChatGitlab company={"Gitlab"} />
-//         <MatchChatInsta company={"Instacart"} />
-//         <MatchChatRemix company={"Remix"} />
-//       </div>
-//     </div>
-//   );
-// }
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   componentDidMount() {
     this.props.getUserMatches(this.props.currentUser.id);
   }
 
+  handleLogout() {
+    this.props.logout();
+    this.props.history.push('/');
+  }
+  
   render() {
     return (
       <div className="navbar">
         <div className="navbar-header">
-          <Link to="/profile"><FaUser className="profile-btn" /></Link>
-          <Link to="/recs"><FaRegAddressCard className="profile-btn matches" /></Link>  
+          <button onClick={this.handleLogout}><BiLogOut className="navbar-header-logout"/></button>
+          <div className="navbar-header-right">
+            <Link to="/profile"><FaUser className="profile-btn" /></Link>
+            <Link to="/recs"><FaRegAddressCard className="profile-btn matches" /></Link> 
+          </div>
+           
         </div>
   
         <div className="match-chats">
@@ -69,4 +58,4 @@ class NavBar extends React.Component {
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
