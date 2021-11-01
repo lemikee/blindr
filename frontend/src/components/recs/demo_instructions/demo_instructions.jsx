499bf2d7f9806267f50b49f8b6e8a21141e253c6
Joshua Zhao <joshua.zhao0315@gmail.com>
2021-11-01 12:20:06 -0700

    Add label to animation

--- frontend/src/components/recs/demo_instructions/demo_instructions.jsx
+++ frontend/src/components/recs/demo_instructions/demo_instructions.jsx
@@ -4,9 +4,12 @@ import {AiOutlineArrowLeft, AiOutlineArrowRight} from 'react-icons/ai'
 const DemoInstructions = () => {
   return (  
     <div className="demo-swipe-animation-container">
-      <AiOutlineArrowLeft className="demo-swipe-arrow left" /> 
-      <h1 className="demo-swipe">Swipe</h1> 
-      <AiOutlineArrowRight className="demo-swipe-arrow right"/>
+      <div className="demo-swipe-animation-top">
+        <AiOutlineArrowLeft className="demo-swipe-arrow left" /> 
+        <h1 className="demo-swipe">Swipe</h1> 
+        <AiOutlineArrowRight className="demo-swipe-arrow right"/>
+      </div>
+      <h1 className="demo-swipe-label"></h1>
     </div>
   );
 }
